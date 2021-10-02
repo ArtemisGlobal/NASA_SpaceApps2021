5557bdc145c53030b88363124219577ebe8bc5e5
MeganVeitz <megan.veitz@gmail.com>
2021-10-02 15:43:52 -0600

    Working SQL database, bugs fixed and Indexes made.

--- ArtemisGlobalSQL.py
+++ ArtemisGlobalSQL.py
@@ -10,7 +10,7 @@ mydatabase = "logs_db"
 connect = None
 try:
     # connect your database and python ide
-    connect = pymysql.connect(user="artemis_user", password="TeamArtemis2021!")
+    connect = pymysql.connect(host='localhost', user="artemis_user", password="TeamArtemis2021!")
     # create cursor object, named cur
     with connect.cursor() as cur:
         # new datatbase
@@ -21,28 +21,38 @@ try:
                     '`username` VARCHAR(30) NOT NULL,\n'
                     '`logdate` DATETIME NOT NULL,\n'
                     '`logtext` TEXT,\n'
-                    '`mediaID` int UNIQUE,\n'
+                    '`mediaID` int,\n'
                     '`hardware` TEXT,\n'
                     '`otherUsername` VARCHAR(30),\n'
                     '`logEdit` DATETIME,\n'
+                    '`hashtag` VARCHAR(30),\n'
+                    '`approvelog` BOOLEAN DEFAULT FALSE,\n'
                     ' PRIMARY KEY (`logID`));')
+        cur.execute(
+            'CREATE INDEX idx_username ON `logs_db`.`logs` (`username`);'
+        )
+
+
+        cur.execute(
+            'CREATE INDEX idx_media ON `logs_db`.`logs` (`mediaID`);'
+        )
 
         cur.execute('CREATE TABLE `logs_db`.`users` (\n'
-                    '`userID` int NOT NULL,\n'
-                    '`username` VARCHAR(30), NOT NULL UNIQUE\n'
-                    '`firstname` TEXT NOT NULL,\n'
-                    '`lastname` TEXT NOT NULL,\n'
-                    '`company` TEXT NOT NULL,\n'
+                    '`username` VARCHAR(30) NOT NULL UNIQUE,\n'
+                    '`firstname` VARCHAR(30) NOT NULL,\n'
+                    '`lastname` VARCHAR(30) NOT NULL,\n'
+                    '`company` VARCHAR(30) NOT NULL,\n'
                     '`approval` BOOLEAN NOT NULL,\n'
-                    ' FOREIGN KEY (`username`) REFERENCES logs(username),\n'
-                    ' PRIMARY KEY (`userID`);')
+                    ' FOREIGN KEY (`username`) REFERENCES `logs` (`username`),\n'
+                    ' PRIMARY KEY (`username`));')
 
         cur.execute('CREATE TABLE `logs_db`.`media` (\n'
-                    '`mediaID` int NOT NULL,\n'
-                    '`imageref` TEXT,\n'
-                    '`videoref` TEXT,\n'
-                    '`audioref` TEXT,\n'
-                    ' FOREIGN KEY (`mediaID`) REFERENCES logs(mediaID);')
+                    '`mediaID` int NOT NULL AUTO_INCREMENT,\n'
+                    '`imageref` VARCHAR(256),\n'
+                    '`videoref` VARCHAR(256),\n'
+                    '`audioref` VARCHAR(256),\n'
+                    ' FOREIGN KEY (`mediaID`) REFERENCES `logs` (`mediaID`),\n'
+                    ' PRIMARY KEY (`mediaID`));')
 
 except Exception as e:
     print("Exception Error")
